3eabe07a2dc5451aca5ec0c7cc08f1d45e326c58
K1rL3s <kirill.lesovoi@mail.ru>
2026-05-02 03:50:58 +0300

    Сваггер на 28.04.2026 и обновление типов

--- src/maxo/types/contact_attachment_payload.py
+++ src/maxo/types/contact_attachment_payload.py
@@ -7,14 +7,27 @@ from maxo.types.user import User
 class ContactAttachmentPayload(MaxoType):
     """
     Args:
+        hash: Хеш информации о пользователе в формате VCF. Используется для проверки того, что пользователь поделился номером телефона, привязанным к его аккаунту в МАКС<br/> Подробнее — [в разделе «Типы кнопок»](https://dev.max.ru/docs-api#Типы%20кнопок)
         max_info: Информация о пользователе
-        vcf_info: Информация о пользователе в формате VCF.
+        vcf_info: Информация о пользователе в формате VCF
     """
 
+    hash: Omittable[str | None] = Omitted()
+    """Хеш информации о пользователе в формате VCF. Используется для проверки того, что пользователь поделился номером телефона, привязанным к его аккаунту в МАКС<br/> Подробнее — [в разделе «Типы кнопок»](https://dev.max.ru/docs-api#Типы%20кнопок)"""
     max_info: Omittable[User | None] = Omitted()
     """Информация о пользователе"""
     vcf_info: Omittable[str | None] = Omitted()
-    """Информация о пользователе в формате VCF."""
+    """Информация о пользователе в формате VCF"""
+
+    @property
+    def unsafe_hash(self) -> str:
+        if is_defined(self.hash):
+            return self.hash
+
+        raise AttributeIsEmptyError(
+            obj=self,
+            attr="hash",
+        )
 
     @property
     def unsafe_max_info(self) -> User:
