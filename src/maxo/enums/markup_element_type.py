afdb278b348f82494ec507f05b13b54e7403218c
K1rL3s <kirill.lesovoi@mail.ru>
2026-05-06 01:30:36 +0300

    Обновление енумов по сваггеру
    - `MarkupElementType`: `QUOTE`, `HEADING` и `HIGHLIGHTED` попали в доку
    - `SenderAction.MARK_SEEN` удалён из доки, но пока остаётся с нами

--- src/maxo/enums/markup_element_type.py
+++ src/maxo/enums/markup_element_type.py
@@ -1,16 +1,14 @@
 from enum import StrEnum
 
 
-# У клиента есть QUOTE (цитата), но в доке не описана и боту не приходит
-# https://t.me/maxo_py/671
 class MarkupElementType(StrEnum):
     EMPHASIZED = "emphasized"
+    HEADING = "heading"
+    HIGHLIGHTED = "highlighted"
     LINK = "link"
     MONOSPACED = "monospaced"
+    QUOTE = "quote"
     STRIKETHROUGH = "strikethrough"
     STRONG = "strong"
     UNDERLINE = "underline"
     USER_MENTION = "user_mention"
-    QUOTE = "quote"  # Нет в доке, работает
-    HEADING = "heading"  # Нет в доке, работает
-    HIGHLIGHTED = "highlighted"  # Нет в доке, работает
