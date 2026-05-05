dd0a7f554a69a2e8b18c78b900e76130621d0ea4
K1rL3s <kirill.lesovoi@mail.ru>
2026-05-06 00:31:41 +0300

    Починил `DeeplinkFilter` с `deep_link_encoded=True`

---
+++ tests/maxo/routing/test_deeplink_filter.py
@@ -0,0 +1,77 @@
+from datetime import UTC, datetime
+
+import pytest
+
+from maxo import Ctx
+from maxo.routing.filters import DeeplinkFilter
+from maxo.routing.updates import BotStarted
+from maxo.types import User
+from maxo.utils.payload import encode_payload
+
+
+def bot_started(payload: str | None) -> BotStarted:
+    return BotStarted(
+        chat_id=1,
+        user=User(
+            first_name="42",
+            is_bot=False,
+            last_activity_time=datetime.fromtimestamp(1234567890, tz=UTC),
+            user_id=42,
+        ),
+        payload=payload,
+        timestamp=datetime.fromtimestamp(1234567890, tz=UTC),
+    )
+
+
+@pytest.mark.asyncio
+async def test_deeplink_is_none() -> None:
+    filter = DeeplinkFilter()
+    started_without_payload = bot_started(payload=None)
+
+    ctx = Ctx({})
+    assert await filter(started_without_payload, ctx) is False
+    assert ctx == {}
+
+
+@pytest.mark.asyncio
+async def test_deeplink_is_empty() -> None:
+    filter = DeeplinkFilter()
+    started_without_payload = bot_started(payload="")
+
+    ctx = Ctx({})
+    assert await filter(started_without_payload, ctx) is False
+    assert ctx == {}
+
+
+@pytest.mark.asyncio
+async def test_deeplink_is_filled() -> None:
+    filter = DeeplinkFilter()
+    started_without_payload = bot_started(payload="helloworld")
+
+    ctx = Ctx({})
+    assert await filter(started_without_payload, ctx) is True
+
+    assert len(ctx) == 3
+    assert ctx["payload"] == ctx["deeplink"] == ctx["args"] == "helloworld"
+
+
+@pytest.mark.asyncio
+async def test_deeplink_is_filled_and_encoded() -> None:
+    filter = DeeplinkFilter(deep_link_encoded=True)
+    started_without_payload = bot_started(payload=encode_payload("helloworld"))
+
+    ctx = Ctx({})
+    assert await filter(started_without_payload, ctx) is True
+
+    assert len(ctx) == 3
+    assert ctx["payload"] == ctx["deeplink"] == ctx["args"] == "helloworld"
+
+
+@pytest.mark.asyncio
+async def test_deeplink_is_filled_and_bad_encoded() -> None:
+    filter = DeeplinkFilter(deep_link_encoded=True)
+    started_without_payload = bot_started(payload="amongus_")
+
+    ctx = Ctx({})
+    assert await filter(started_without_payload, ctx) is False
+    assert ctx == {}
