baf0ee5be3d9aa8bbba5b1537175ce1355cf5c64
Egor Begeza <begezyan@gmail.com>
2026-05-30 16:02:10 +0500

    fix: test_aiohttp_adapter

--- tests/maxo_webhook/test_aiohttp_adapter.py
+++ tests/maxo_webhook/test_aiohttp_adapter.py
@@ -2,6 +2,8 @@ from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 from aiohttp import web
+from aiohttp.pytest_plugin import AiohttpClient
+from aiohttp.web_app import Application
 
 from maxo.transport.webhook.adapters.aiohttp.adapter import (
     AiohttpBoundRequest,
@@ -10,29 +12,39 @@ from maxo.transport.webhook.adapters.aiohttp.adapter import (
 
 
 @pytest.fixture
-def aiohttp_app():
+def aiohttp_app() -> web.Application:
     return web.Application()
 
 
 @pytest.fixture
-def mocked_engine():
+def mocked_engine() -> MagicMock:
     engine = MagicMock()
     engine.feed_request = AsyncMock()
     return engine
 
 
-@pytest.mark.skip("Разобраться с ошибкой")
 @pytest.mark.asyncio
-async def test_adapter(aiohttp_client, aiohttp_app):
-    engine = AsyncMock(return_value=web.Response(status=200))
+async def test_adapter(aiohttp_client: AiohttpClient, aiohttp_app: Application) -> None:
+    payload = None
+
+    async def handler(request: AiohttpBoundRequest) -> web.Response:
+        nonlocal payload
+
+        assert isinstance(request, AiohttpBoundRequest)
+        payload = await request.json()
+        return web.Response(status=200)
+
+    engine = AsyncMock(side_effect=handler)
 
     adapter = AiohttpWebAdapter()
     adapter.register(aiohttp_app, "/webhook", engine)
 
     client = await aiohttp_client(aiohttp_app)
-    await client.post("/webhook", json={"foo": "bar"})
+    response = await client.post("/webhook", json={"foo": "bar"})
+    assert response.status == 200
+    await response.read()
 
     engine.assert_awaited_once()
     request = engine.call_args.args[0]
     assert isinstance(request, AiohttpBoundRequest)
-    assert await request.json() == {"foo": "bar"}
+    assert payload == {"foo": "bar"}
